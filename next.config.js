e7894e05093a6f784769a9f1c6ee61301acf8229
Eman59 <sayyedeman59@gmail.com>
2022-05-29 13:50:14 +0530

    preact removed

--- next.config.js
+++ next.config.js
@@ -15,11 +15,6 @@ module.exports = {
             test: /\.svg$/,
             use: ["@svgr/webpack"]
         });
-        Object.assign(config.resolve.alias, {
-            react: 'preact/compat',
-            'react-dom/test-utils': 'preact/test-utils',
-            'react-dom': 'preact/compat',
-        });
 
         return config;
     }
